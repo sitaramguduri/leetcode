46d4f01c8d1c1eba9fc8877e362b13d72cec8e8f
Ram <nagasita@buffalo.edu>
2025-01-18 03:02:08 -0500

    Update file 18/01/2025, 03:02:08

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.cpp
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.cpp
@@ -8,16 +8,33 @@ public:
         }
         return res;
     }
+    vector<int> pascalTriangle(int row){
+        long long int res = 1;
+        vector<int> eachRow;
+        eachRow.push_back(res);
+        for(int i = 1;i<row;i++){
+            res = res*(row-i);
+            res = res/(i);
+            // cout<<res<<" ";
+            eachRow.push_back(res);
+        }
+        cout<<endl;
+        return eachRow;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i = 0; i<numRows;i++){
-            //for each row print all rows
-            vector<int> eachRow ;
-            for(int j = 0; j<=i;j++){
-                int res = ncr(i,j);
-                eachRow.push_back(res);
-            }
-            ans.push_back(eachRow);
+        // for(int i = 0; i<numRows;i++){
+        //     //for each row print all rows
+        //     vector<int> eachRow ;
+        //     for(int j = 0; j<=i;j++){
+        //         int res = ncr(i,j);
+        //         eachRow.push_back(res);
+        //     }
+        //     ans.push_back(eachRow);
+        // }
+        for(int i = 1;i<=numRows;i++){
+           vector<int> res =  pascalTriangle(i);
+           ans.push_back(res);
         }
         return ans;
     }
