2a14cf476fb32d8f24298841f2d4cfd5b426839a
Ram <nagasita@buffalo.edu>
2025-01-24 01:54:27 -0500

    Create file

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int count(int i, int j, int m , int n, vector<vector<int>> &dp){
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(i>m || j > n ){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] = count(i+1,j,m,n,dp) + count(i,j+1,m,n,dp);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>( n+1,-1));
+        return count(0,0,m,n,dp);
+    }
+};
