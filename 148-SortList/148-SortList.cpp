2d406cd76ab14cecaeab2f2672e4faa31deba1dd
Ram <nagasita@buffalo.edu>
2025-01-29 12:00:33 -0500

    Create file

---
+++ 148-SortList/148-SortList.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* left, ListNode* right){
+        ListNode* newHead = new ListNode(-1);
+        ListNode* temp = newHead;
+        while(left!=nullptr && right != nullptr){
+            if(left->val < right->val){
+                newHead->next = left;
+                left = left->next;
+
+            }else{
+                newHead->next = right;
+                right = right->next;
+            }
+            newHead = newHead->next;
+        }
+        while(left!=nullptr){
+            newHead->next = left;
+            left = left->next;
+            newHead = newHead->next;
+        }
+        while(right!=nullptr){
+            newHead->next = right;
+            right = right->next;
+            newHead = newHead->next;
+        }
+        // ListNode* t = temp;
+        // while(t!=nullptr){
+        //     cout<<t->val<<" ";
+        //     t = t->next;
+        // }
+        // cout<<endl;
+        return temp->next;
+
+    }
+    ListNode* findMiddle(ListNode* head){
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode* slow= head;
+        ListNode* fast = head->next;
+        while( fast!=NULL && fast->next != NULL ){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* left = head;
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        left = sortList(left);
+        right = sortList(right);
+        // return NULL;
+        return merge(left,right);
+    }
+};
