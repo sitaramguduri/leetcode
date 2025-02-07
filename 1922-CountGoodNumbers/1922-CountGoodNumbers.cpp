bee46997e4a643094b8c6371bba4c4cdf7cabbed
Ram <nagasita@buffalo.edu>
2025-02-06 19:10:41 -0500

    Create file

---
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.cpp
@@ -0,0 +1,27 @@
+#define mod 1000000007
+class Solution {
+public:
+    long long int helper(int x, long long int n){
+        if(n==0){
+            return (double) (1.0);
+        }
+        long long int half = helper(x,n/2);
+        long long int ans = half* half;
+        ans = ans%mod;
+        return n%2==0?(ans)%mod:(ans*x)%mod;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long int res = 1;
+        if( n%2 == 0){
+            long long int half =(int)( helper(5,n/2))%mod;
+            long long int odd = (int) helper(4,n/2)%mod;
+            res = (half*odd)%mod;
+        }else{
+            long long int half =(int) helper(5,n/2 + 1);
+            long long int odd = (int)helper(4,n/2);
+            res = (half*odd)%mod;
+        }
+        return res;
+    }
+};
