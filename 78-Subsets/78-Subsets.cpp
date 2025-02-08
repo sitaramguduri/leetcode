212b13584ccf22c726495e24ba19e0ed7296876d
Ram <nagasita@buffalo.edu>
2025-02-07 19:06:05 -0500

    Create file

---
+++ 78-Subsets/78-Subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(vector<int>& nums, int i , vector<vector<int>> & res,vector<int> temp){
+        if(i == nums.size() ){
+            res.push_back(temp);
+            return ;
+        }
+        // add the element to the temp
+        temp.push_back(nums[i]);
+        helper(nums, i+1,res,temp);
+        temp.pop_back();
+        helper(nums, i+1, res, temp);
+        return;
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res ;
+        int i = 0;
+        vector<int> temp;
+        helper(nums,i,res, temp);
+        return res;
+    }
+};
