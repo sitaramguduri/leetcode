54a4ccf267aa9269b05d89254625033bc384fde7
Ram <nagasita@buffalo.edu>
2025-03-06 13:46:06 -0500

    File updated at 06/03/2025, 13:46:06

--- 39-CombinationSum/39-CombinationSum.cpp
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void helper(vector<int>& candidates, int target, vector<int> &res, int i, vector<vector<int>> &fin){
-        // if(i == candidates.size()){
-            if(target == 0){
-                fin.push_back(res);
-                return;
-            }
-        // }
-        if(i >= candidates.size()||target<0 ){
-            
-            return ;
+    void helper(vector<int>& candidates, int target, vector<int> &res, int i, vector<vector<int>> &fin,int sum){
+        if(sum>target){
+            return;
         }
+        if(i == candidates.size()){
+            if(target == sum){
+                fin.push_back(res);
+                // return;
+            }
+            return;
+        }
+        
         res.push_back(candidates[i]);
         
-        helper(candidates, target-candidates[i], res,i, fin);
+        helper(candidates, target, res,i, fin,sum+candidates[i]);
         res.pop_back();
         
-        helper(candidates, target, res, i + 1, fin);
+        helper(candidates, target, res, i + 1, fin,sum);
         return;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int sum = 0;
         vector<int> res;
         vector<vector<int>> fin;
-        helper(candidates, target, res, 0 ,fin);
+        helper(candidates, target, res, 0 ,fin,sum);
         return fin;
     }
 };
