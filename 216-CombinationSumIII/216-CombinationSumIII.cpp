e6fccf120e1b9d71753bbaa0b62a6a8c418db209
Ram <nagasita@buffalo.edu>
2025-03-06 21:33:45 -0500

    File updated at 06/03/2025, 21:33:45

--- 216-CombinationSumIII/216-CombinationSumIII.cpp
+++ 216-CombinationSumIII/216-CombinationSumIII.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    void helper(int k , int n, vector<int>& temp, vector<vector<int>>& fin, int ind,int* arr){
-        if( n == 0 & k==0){
-            fin.push_back(temp);
+    void helper(int index, int k, int n, vector<int>& temp, vector<vector<int>>& res) {
+        if (n == 0 && temp.size() == k) { // Base Case: Valid combination found
+            res.push_back(temp);
             return;
         }
-        if (n < 0 || k < 0) return;
-        for(int i = ind; i< 10; i++){
-            if(i>n){
-                // continue;
-                break;
-            }
-            temp.push_back(i);
-            helper(k-1,n-i,temp,fin,i+1,arr);
-            temp.pop_back();
-        }
-        return ;
+        if (index > 9 || temp.size() > k) return; // If we exceed valid numbers or size
+
+        // Not Take: Skip current number
+        helper(index + 1, k, n, temp, res);
+
+        // Take: Include current number
+        temp.push_back(index);
+        helper(index + 1, k, n - index, temp, res);
+        temp.pop_back(); // Backtrack
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        int* arr = new int[10];
-        for(int i = 0; i< 10;i++){
-            arr[i] = i;
-        }
-        vector<int> temp ;
-        vector<vector<int>> fin;
-        helper(k,n, temp, fin,1,arr);
-        return fin;
+        vector<vector<int>> res;
+        vector<int> temp;
+        helper(1, k, n, temp, res);
+        return res;
     }
 };
