a13dc00fddc92480fd9b5096f576e85abe12dbe9
Ram <nagasita@buffalo.edu>
2025-03-16 00:15:47 -0400

    File updated at 16/03/2025, 00:15:46

--- 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.cpp
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.cpp
@@ -13,26 +13,29 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result;
-        if(root == NULL){
-            return result;
-        }
-        stack<TreeNode*> s1,s2;
-        s1.push(root);
-        while(!s1.empty()){
-            TreeNode* node = s1.top();
-            s1.pop();
-            s2.push(node);
-            if(node->left != NULL){
-                s1.push(node->left);
+        TreeNode* cur = root;
+        stack<TreeNode*> st;
+        while(cur !=NULL || !st.empty()){
+            if(cur!=NULL){
+                st.push(cur);
+                cur = cur->left;
+            }else{
+                TreeNode* temp = st.top()->right;
+                if(temp== NULL){
+                    temp = st.top();
+                    st.pop();
+                    result.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        result.push_back(temp->val);
+
+                    }
+                }else{
+                    cur = temp;
+                }
             }
-            if(node->right != NULL){
-                s1.push(node->right);
-            }
         }
-        while(!s2.empty()){
-            result.push_back(s2.top()->val);
-            s2.pop();
-        }
-        return result;
+        return result;   
     }
 };
