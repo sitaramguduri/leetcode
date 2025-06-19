5df3591d9049110086bef7eecf31ad180d425e40
Ram <nagasita@buffalo.edu>
2025-06-18 23:52:48 -0400

    Create 26-RemoveDuplicatesfromSortedArray

---
+++ 26-RemoveDuplicatesfromSortedArray/version/cpp/26-RemoveDuplicatesfromSortedArray_20250618_235248.cpp
@@ -0,0 +1,21 @@
+// Last updated: 18/06/2025, 23:52:48
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // optimal approach : Time complexity:
+        int i = 0;
+        int j = 1;
+        while(j < nums.size()){
+            if(nums[i] == nums[j]){
+                j++;
+                continue;
+            }else{
+                nums[i+1] = nums[j];
+                i++;
+                 j++;
+            }
+        }
+        return i+1;
+        
+    }
+};
