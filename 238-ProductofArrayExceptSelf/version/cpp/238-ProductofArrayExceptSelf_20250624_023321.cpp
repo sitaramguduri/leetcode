cf39d0f77dc19b53bb2297bb3d0348532ca3a28d
Ram <nagasita@buffalo.edu>
2025-06-24 02:33:22 -0400

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/version/cpp/238-ProductofArrayExceptSelf_20250624_023321.cpp
@@ -0,0 +1,31 @@
+// Last updated: 24/06/2025, 02:33:21
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // basic approach: O(N^2)
+        int prefixProduct = 1;
+        int suffixProduct = 1; 
+        int n = nums.size();
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        for(int i = 0; i< nums.size(); i++){
+            // cout<<"prefix: "<< prefixProduct<<endl;
+             prefix[i] = prefixProduct;
+             prefixProduct *= nums[i];
+        }
+        for(int i = nums.size()-1; i >= 0; i--){
+            // cout<< " suffix: "<<suffixProduct<<endl;
+             prefix[i] =prefix[i]* suffixProduct;
+             suffixProduct *= nums[i];
+        }
+        // for(int i = 0; i< n ; i++){
+        //     cout<< prefix[i]<<" pre:"<< endl;
+        //     cout<< suffix[n-i]<< " suffix: "<<endl;
+        // }
+        // vector<int> result(n);
+        // for(int i = 0; i< nums.size(); i++){
+        //     result[i] = prefix[i]*suffix[i];
+        // }
+        return prefix;
+    }
+};
