a30317dffb170d4c4586ccd287b95b861de543f4
Ram <nagasita@buffalo.edu>
2025-06-27 13:47:54 -0400

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/cpp/42-TrappingRainWater_20250627_134754.cpp
@@ -0,0 +1,34 @@
+// Last updated: 27/06/2025, 13:47:54
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
+        int maxL = height[0];
+        int maxR = height[height.size()-1];
+        int i = 0;
+        int result = 0;
+        while(l < r){
+            if(height[l] <= height[r]){
+                i = l;
+            }else{
+                i = r;
+            }
+            int minHeight = min(maxL, maxR);
+            int water = minHeight - height[i];
+            if (water < 0){
+                water = 0;
+            }
+            result += water;
+            maxL = max(maxL, height[i]);
+            maxR = max(maxR, height[i]);
+            if(height[l] <= height[r]){
+                l++;
+            }else{
+                r--;
+            }
+
+        }
+        return result;
+    }
+};
