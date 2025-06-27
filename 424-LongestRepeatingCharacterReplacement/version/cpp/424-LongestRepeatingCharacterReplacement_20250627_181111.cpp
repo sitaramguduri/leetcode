2f681917fa7f15af3aac7cf52418da3dc813ad6c
Ram <nagasita@buffalo.edu>
2025-06-27 18:11:11 -0400

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/version/cpp/424-LongestRepeatingCharacterReplacement_20250627_181111.cpp
@@ -0,0 +1,28 @@
+// Last updated: 27/06/2025, 18:11:11
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int r = 0;
+        map<char, int> mp;
+        int maxf = 0;
+        int maxlen = 0;
+        while(r < s.size()){
+            mp[s[r]] ++;
+            maxf = max(maxf, mp[s[r]]);
+            if( (r - l + 1 ) - maxf >k){
+                mp[s[l]]--;
+                maxf = 0;
+                for(auto i: mp){
+                    maxf = max(maxf, i.second);
+                }
+                l++;
+            }
+            if((r-l+1) - maxf <=k){
+                maxlen = max(maxlen, r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
