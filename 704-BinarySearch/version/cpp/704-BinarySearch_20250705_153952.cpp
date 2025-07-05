464cbdaeb7d296bcf1a5dcc8f7aa1b54a50606b7
Ram <nagasita@buffalo.edu>
2025-07-05 15:39:52 -0400

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/cpp/704-BinarySearch_20250705_153952.cpp
@@ -0,0 +1,23 @@
+// Last updated: 05/07/2025, 15:39:52
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int result = -1;
+        int left = 0;
+        int right = nums.size() -1 ;
+        while(left <= right){
+            int middle = (left + right)/2;
+            // cout<< middle << endl;
+            if(target < nums[middle] ){
+                right = middle-1;
+            }else if(target > nums[middle]){
+                left = middle + 1;
+            }else{
+                result = middle;
+                return result;
+            }
+        }
+        return result;
+
+    }
+};
