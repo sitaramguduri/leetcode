2241a591815176cfe006e32f3db3e9c5f92b7db8
Ram <nagasita@buffalo.edu>
2025-07-05 17:16:02 -0400

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/version/cpp/875-KokoEatingBananas_20250705_171602.cpp
@@ -0,0 +1,39 @@
+// Last updated: 05/07/2025, 17:16:02
+class Solution {
+public:
+    bool eatInTime(int k, int h , vector<int> piles){
+        long timeTaken = 0;
+        for(int i = 0; i< piles.size(); i++){
+            long temp = ceil(1.0 * piles[i] / k);
+            // cout<<"temp: "<< temp<<endl;
+            timeTaken += temp;
+        }
+        // cout<< "timeTaken: "<< timeTaken<< endl;
+        if(timeTaken > h){
+            return false;
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = INT_MIN;
+        for(int i = 0; i< piles.size(); i++){
+            r = max(r, piles[i]);
+        }
+        int m;
+        int res = INT_MAX;
+        while(l <= r){
+            m = (l + r)/2;
+            // cout << "m: "<< m<<endl;
+            // cout<<"l: "<< l << " r: "<< r<< endl;
+            if(eatInTime(m, h,piles)){
+                r = m -1;
+                res = min(res, m);
+            }else{
+                l = m+ 1;
+            }
+        }
+        return res;
+        
+    }
+};
