73c3be6526e7ef2f8e402c9e5b11adc2f7380a49
Ram <nagasita@buffalo.edu>
2025-07-17 20:52:08 -0400

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,24 @@
+// Last updated: 17/07/2025, 20:52:08
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        vector<vector<int>> freq(nums.size() + 1);
+        for(int i = 0; i< nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto it: mp){
+            freq[it.second].push_back(it.first);
+        }
+        vector<int> res;
+        for(int i = freq.size()-1; i>=0; i--){
+            for(auto n: freq[i]){
+                res.push_back(n);
+            }
+            if(res.size() == k){
+                break;
+            }
+        }
+        return res;
+    }
+};
