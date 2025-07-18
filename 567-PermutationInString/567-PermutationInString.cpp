9cdd27d7026615a24f308d4ab36295b37c4653f6
Ram <nagasita@buffalo.edu>
2025-07-17 20:52:00 -0400

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.cpp
@@ -0,0 +1,31 @@
+// Last updated: 17/07/2025, 20:51:59
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size() < s1.size()){
+            return false;
+        }
+        unordered_map<char,int> mp1;
+        unordered_map<char, int> mp2;
+        for(int i = 0; i< s1.size(); i++){
+            mp1[s1[i]]++;
+        }
+        int l = 0; 
+        int r = 0; 
+        bool result = false;
+        for(int r = 0; r< s2.size();r++){
+            mp2[s2[r]]++;
+            if( r - l + 1 > s1.size()){
+                mp2[s2[l]]--;
+                if(mp2[s2[l]] == 0){
+                    mp2.erase(s2[l]);
+                }
+                l++;
+            }
+            if(mp1 == mp2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
