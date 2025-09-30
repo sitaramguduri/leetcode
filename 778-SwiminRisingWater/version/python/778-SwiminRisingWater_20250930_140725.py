15494beff2d6d600d66921920b96daf34eb0a437
Ram <nagasita@buffalo.edu>
2025-09-30 14:07:26 -0400

    Create 778-SwiminRisingWater

---
+++ 778-SwiminRisingWater/version/python/778-SwiminRisingWater_20250930_140725.py
@@ -0,0 +1,21 @@
+# Last updated: 30/09/2025, 14:07:25
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        minHeap = [(grid[0][0],0,0)]
+        visited = set()
+        visited.add((0,0))
+        result = 0
+        direction = [(1,0),(-1,0),(0,1),(0,-1)]
+
+        while minHeap:
+            elevation,r,c = heapq.heappop(minHeap)
+            result = max(result,elevation)
+            if r == n-1 and c == n-1:
+                return result
+            for dr,dc in direction:
+                nr,nc = r + dr, c +dc
+                if 0<= nr < n and 0 <= nc < n and (nr,nc) not in visited:
+                    visited.add((nr,nc))
+                    heapq.heappush(minHeap,(grid[nr][nc],nr,nc))
+        return -1
