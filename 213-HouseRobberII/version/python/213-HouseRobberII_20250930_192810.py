23a6adc0a6c81b4eec0dcbfeb9bc6a4276a3a59e
Ram <nagasita@buffalo.edu>
2025-09-30 19:28:11 -0400

    Create 213-HouseRobberII

---
+++ 213-HouseRobberII/version/python/213-HouseRobberII_20250930_192810.py
@@ -0,0 +1,22 @@
+# Last updated: 30/09/2025, 19:28:10
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        def robbing(houses:List[int]) -> int:
+            length = len(houses)
+            if length == 0:
+                return 0
+            if length == 1:
+                return houses[0]
+            prev1 = max(houses[0],houses[1])
+            prev2 = houses[0]
+            for i in range(2,length):
+                current = max(prev1,prev2+houses[i])
+                prev2 = prev1
+                prev1 = current
+            return prev1
+        option1 = robbing(nums[:-1])
+        option2 = robbing(nums[1:])
+        return max(option1,option2)
