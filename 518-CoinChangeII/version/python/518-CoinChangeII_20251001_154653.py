fdd6e5a551aee9c81a1e7c8fe78828a1f27ea2d7
Ram <nagasita@buffalo.edu>
2025-10-01 15:46:53 -0400

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/version/python/518-CoinChangeII_20251001_154653.py
@@ -0,0 +1,10 @@
+# Last updated: 01/10/2025, 15:46:53
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x-coin]
+        return dp[amount]
+        
