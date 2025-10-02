6719e870f5445fa40498ba4786d553666f5f03f3
Ram <nagasita@buffalo.edu>
2025-10-02 01:07:34 -0400

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/version/python/115-DistinctSubsequences_20251002_010734.py
@@ -0,0 +1,15 @@
+# Last updated: 02/10/2025, 01:07:34
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m = len(s)
+        n = len(t)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][n] = 1
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+                else:
+                    dp[i][j] = dp[i+1][j]
+        return dp[0][0]
