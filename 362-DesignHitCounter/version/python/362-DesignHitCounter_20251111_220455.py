b065b1eb41c79068a9ec8217b0deba8857153c11
Ram <nagasita@buffalo.edu>
2025-11-11 22:04:55 -0500

    Create 362-DesignHitCounter

---
+++ 362-DesignHitCounter/version/python/362-DesignHitCounter_20251111_220455.py
@@ -0,0 +1,22 @@
+# Last updated: 11/11/2025, 22:04:55
+class HitCounter:
+
+    def __init__(self):
+        self.hits = deque()
+
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+        
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits and self.hits[0] <= timestamp - 300:
+            self.hits.popleft()
+        return len(self.hits)
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
